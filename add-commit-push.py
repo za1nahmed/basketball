6f96b0098c138fd97dd12071b85cac202fab89c6
za1nahmed <zainahmed@lewisu.edu>
2025-04-02 10:43:24 -0500

    updated add-commit-push.py

---
+++ add-commit-push.py
@@ -0,0 +1,25 @@
+import subprocess
+
+print("Add Commit Push")
+
+
+# Print the output
+print("Executing \"git status\":")
+print("")
+
+# Run the git status command
+resultGitStatus = subprocess.run(["git", "status"], capture_output=True, text=True)
+
+print(resultGitStatus.stdout)
+#print("STDERR:") 
+#print(result.stderr)
+
+print("Executing \"git add -A\":")
+print("")
+
+# Run the git status command
+resultGitAdd = subprocess.run(["git", "add -A"], capture_output=True, text=True)
+
+print(resultGitAdd.stdout)
+print("STDERR:") 
+print(resultGitAdd.stderr)
