7e759d0a8b745eae8774040d2745a6f7bff9e026
za1nahmed <zainahmed@lewisu.edu>
2025-04-04 10:55:01 -0500

    Add line spaceing.

--- add-commit-push.py
+++ add-commit-push.py
@@ -11,13 +11,11 @@ print(resultGitStatus.stdout)
 
 # Executing git add.
 print("Executing \"git add -A\":")
-print("")
 
 # Run the git add command
 resultGitAdd = subprocess.run(["git", "add", "-A"], capture_output=True, text=True)
 print(resultGitAdd.stdout)
-print("STDERR:") 
-print(resultGitAdd.stderr)
+print("")
 
 # Executing git commit.
 print("Executing \"git commit -m\":")
@@ -31,8 +29,7 @@ if len(sys.argv) == 3:
 
 resultGitCommit = subprocess.run(["git", "commit", "-m", message], capture_output=True, text=True)
 print(resultGitCommit.stdout)
-print("STDERR:") 
-print(resultGitCommit.stderr)
+print("")
 
 # Executing git push.
 print("Executing \"git push\":")
@@ -41,8 +38,4 @@ print("")
 # Run the git push command
 resultGitPush = subprocess.run(["git", "push"], capture_output=True, text=True)
 print(resultGitPush.stdout)
-print("STDERR:") 
-print(resultGitPush.stderr)
-
-#print("STDERR:") 
-#print(result.stderr)
+print("")
